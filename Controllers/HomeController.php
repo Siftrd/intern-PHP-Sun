4ead7a8d507abb1e643e1bd1cac811cc11a11fbb
phongthieu <72301235+phong24021909@users.noreply.github.com>
2023-07-18 10:00:03 +0700

    feature: validate input

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,13 +1,16 @@
 <?php
 
-Class HomeController {
+class HomeController
+{
     private $db;
 
-    public function __construct(Database $db) {
+    public function __construct(Database $db)
+    {
         $this->db = $db;
     }
 
-    public function index() {
+    public function index()
+    {
         $cookie_name = 'Acc';
         $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_SPECIAL_CHARS);
         if (empty($_SESSION["user"])) {
@@ -54,10 +57,24 @@ Class HomeController {
 
                 case "create_movie":
                     if (isset($_POST['insert_movie'])) {
+                        $title = $_POST['title'];
+                        $time = $_POST['time'];
+                        $type = $_POST['type'];
+
+                        // Validate input
+                        if (!$this->validateInput($title, $time, $type)) {
+                            // Invalid input, display error message
+                            echo '<script type="text/javascript">';
+                            echo 'alert("Invalid input. Please check your input and try again.");';
+                            echo '</script>';
+                            require "views/create_movie.php";
+                            break;
+                        }
+
                         $movie = new Movie();
-                        $movie->setTitle($_POST['title']);
-                        $movie->setTime($_POST['time']);
-                        $movie->setType($_POST['type']);
+                        $movie->setTitle($title);
+                        $movie->setTime($time);
+                        $movie->setType($type);
                         $insert_success_movie = $this->createmovie($movie);
                         header('Location: /index.php?page=show&insert_success_movie=' . (bool)$insert_success_movie . '&id=' . $movie->getId());
                         exit();
@@ -75,6 +92,22 @@ Class HomeController {
 
                 case "do_update_movie":
                     if (isset($_POST['update_movie'])) {
+                        $title = $_POST['title'];
+                        $time = $_POST['time'];
+                        $type = $_POST['type'];
+
+                        // Validate input
+                        if (!$this->validateInput($title, $time, $type)) {
+                            // Invalid input, display error message
+                            echo '<script type="text/javascript">';
+                            echo 'alert("Invalid input. Please check your input and try again.");';
+                            echo '</script>';
+                            $id = $_POST['id'];
+                            $movie = $this->db->getById('movie', $id);
+                            require "views/update_movie.php";
+                            break;
+                        }
+
                         $movie = new Movie($_POST);
                         $update_success_movie = $this->updatemovie($movie);
                         header('Location: /index.php?page=show&update_success_movie=' . (int)$update_success_movie . '&id=' . $movie->getId());
@@ -105,20 +138,24 @@ Class HomeController {
         }
     }
 
-    public function updatemovie(Movie $movie) {
+    public function updatemovie(Movie $movie)
+    {
         return $this->db->update('movie', $movie->getId(), $movie->toArray());
     }
 
-    public function createmovie(Movie $movie) {
+    public function createmovie(Movie $movie)
+    {
         return $this->db->create('movie', $movie->toArray());
     }
 
-    public function deletemovie($id) {
+    public function deletemovie($id)
+    {
         $this->db->delete('movie', $id);
         $this->deleteSuccess();
     }
 
-    public function success() {
+    public function success()
+    {
         if (isset($_GET['insert_success_movie'])) {
             if ($_GET['insert_success_movie']) {
                 echo '<script type="text/javascript">';
@@ -132,7 +169,8 @@ Class HomeController {
         }
     }
 
-    public function updateSuccess() {
+    public function updateSuccess()
+    {
         if (isset($_GET['update_success_movie'])) {
             if ($_GET['update_success_movie']) {
                 echo '<script type="text/javascript">';
@@ -146,7 +184,8 @@ Class HomeController {
         }
     }
 
-    public function deleteSuccess() {
+    public function deleteSuccess()
+    {
         if (isset($_GET['delete_success'])) {
             echo '<script type="text/javascript">';
             echo 'alert("Movie deleted successfully!");';
@@ -154,5 +193,22 @@ Class HomeController {
             echo '</script>';
             exit();
         }
+    }
+
+    private function validateInput($title, $time, $type)
+    {
+        if (strlen($title) < 3 || strlen($title) > 100) {
+            return false;
+        }
+
+        if (strlen($time) < 3 || strlen($time) > 100) {
+            return false;
+        }
+
+        if (!in_array($type, ['Action', 'Adventure', 'Comedy', 'Drama', 'Horror', 'Romance', 'Sci-Fi'])) {
+            return false;
+        }
+
+        return true;
     }
 }
